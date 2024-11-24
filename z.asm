a5da7930e2f14dd3cae002a763fb0ae2dc8a6105
Kooky Warrior <kookywarrior@gmail.com>
2024-11-24 15:06:57 +0800

    a

--- z.asm
+++ z.asm
@@ -2,65 +2,44 @@
 .stack 100h
 .data
     ; Fruit Store Title
-    line   db '=-=-=-=-=-=-=-=-=-=-=-=-=-=-$'
-    apple1 db "   ,--./,-.   $"
-    apple2 db "  / #      \  $"
-    apple3 db " |          | $"
-    apple4 db "  \        /  $"
-    apple5 db "   `._,._,'   $"
-    storeName db '      ABC FRUIT STORE$      '
+    line        db '=-=-=-=-=-=-=-=-=-=-=-$'
+    apple1      db "       ,--./,-.       $"
+    apple2      db "      / #      \      $"
+    apple3      db "     |          |     $"
+    apple4      db "      \        /      $"
+    apple5      db "       `._,._,'       $"
+    appleString dw offset apple1, offset apple2, offset apple3, offset apple4, offset apple5
+    storeName   db '  FRESH FRUIT STORE  $'
 
     ; System Shutdown
     shutdown    db 'Shutting down...$'
     
-    ; Fruits Name (grapes, apple, orange, papaya, watermelon, strawberry, pear, guava)
-    fGrapes     db 'Grapes$'
-    fApple      db 'Apple$'
-    fOrange     db 'Orange$'
-    fPapaya     db 'Papaya$'
-    fWatermelon db 'Watermelon$'
-    fStrawberry db 'Strawberry$'
-    fPear       db 'Pear$'
-    fGuava      db 'Guava$'
+    ; Fruits (grapes 7, apple 2.2, orange 1.8, papaya 5, watermelon 18, strawberry 9, pear 4.5, guava 2.5)
+    pointerGrapes     db 'Grapes      $'
+    pointerApple      db 'Apple       $'
+    pointerOrange     db 'Orange      $'
+    pointerPapaya     db 'Papaya      $'
+    pointerWatermelon db 'Watermelon  $'
+    pointerStrawberry db 'Strawberry  $'
+    pointerPear       db 'Pear        $'
+    pointerGuava      db 'Guava       $'
+    fruitsName dw offset pointerGrapes, offset pointerApple, offset pointerOrange, offset pointerPapaya, offset pointerWatermelon, offset pointerStrawberry, offset pointerPear, offset pointerGuava
+    fruitsIPrice db 7, 2, 1, 5, 18, 9, 4, 2
+    fruitsFprice db 0, 20, 80, 0, 0, 0, 50, 50
 
-    ; Fruits Price
-    pGrapes     dw 10
-    pApple      dw 2
-    pOrange     dw 3
-    pPapaya     dw 7
-    pWatermelon dw 15
-    pStrawberry dw 10
-    pPear       dw 3
-    pGuava      dw 5
-
-    ; Fruits Total Sold
-    tGrapes     dw 0
-    tApple      dw 0
-    tOrange     dw 0
-    tPapaya     dw 0
-    tWatermelon dw 0
-    tStrawberry dw 0
-    tPear       dw 0
-    tGuava      dw 0
-
-    ; Cart fruits quantity
-    cGrapes     dw 0
-    cApple      dw 0
-    cOrange     dw 0
-    cPapaya     dw 0
-    cWatermelon dw 0
-    cStrawberry dw 0
-    cPear       dw 0
-    cGuava      dw 0
+    ; Cart
+    cart db 8 dup(0)
 
     ; Plastic Bag
-    pBag        dw 1
-    PBagString  db 'Plastic Bag$'
+    pFBag       db 50 ;RM0.50
+    PBagString  db 'Plastic Bag $'
 
     ; Summary Report
-    totalProfit dw 0
+    fruitsSold db 8 dup(0)
+    totalProfitI dw 0
+    totalProfitF dw 0
 
-    ;==============================================================================
+;==============================================================================
 .code
 main proc
     ; Initialize data segment
@@ -68,21 +47,29 @@ main proc
     mov ds, ax
 
     loginLoop:
-    ; Print Apple
-    call printApple
-    
-    mov dx, offset line
-    mov ah, 09h
-    int 21h
-    call newline
-    mov dx, offset storeName
-    mov ah, 09h
-    int 21h
-    call newline
-    mov dx, offset line
-    mov ah, 09h
-    int 21h
-
+        ; Print Fruit Store Title
+        call newline
+        mov dx, offset line
+        mov ah, 09h
+        int 21h
+        call newline
+        mov si, offset appleString
+        mov cx, 5
+        apple:
+            mov dx, [si]
+            mov ah, 09h
+            int 21h
+            call newline
+            add si, 2
+        loop apple
+        mov dx, offset storeName
+        mov ah, 09h
+        int 21h
+        call newline
+        mov dx, offset line
+        mov ah, 09h
+        int 21h
+        call newline
    
     ; Print Shutdown Message
     call newline
@@ -96,45 +83,6 @@ main proc
     int 21h
 main endp
 
-printApple proc
-    mov cx, 2
-    printApple1:
-    mov dx, offset apple1
-    mov ah, 09h
-    int 21h
-    loop printApple1
-    call newline
-    mov cx, 2
-    printApple2:
-    mov dx, offset apple2
-    mov ah, 09h
-    int 21h
-    loop printApple2
-    call newline
-    mov cx, 2
-    printApple3:
-    mov dx, offset apple3
-    mov ah, 09h
-    int 21h
-    loop printApple3
-    call newline
-    mov cx, 2
-    printApple4:
-    mov dx, offset apple4
-    mov ah, 09h
-    int 21h
-    loop printApple4
-    call newline
-    mov cx, 2
-    printApple5:
-    mov dx, offset apple5
-    mov ah, 09h
-    int 21h
-    loop printApple5
-    call newline
-    ret
-printApple endp
-
 newline proc
     mov ah, 02h
     mov dl, 0Dh
@@ -143,5 +91,12 @@ newline proc
     int 21h
     ret
 newline endp
+
+tab proc
+    mov ah, 02h
+    mov dl, 09h
+    int 21h
+    ret
+tab endp
 
 end main
