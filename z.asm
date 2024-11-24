f961594facadb597cbfcedf6b8ebaddba34709cb
Kooky Warrior <kookywarrior@gmail.com>
2024-11-25 00:32:32 +0800

    a

--- z.asm
+++ z.asm
@@ -12,7 +12,7 @@
     storeName   db '  FRESH FRUIT STORE  $'
 
     ; System Shutdown
-    shutdown    db 'Shutting down...$'
+    shutdown db 'Shutting down...$'
     
     ; Fruits (grapes 7, apple 2.2, orange 1.8, papaya 5, watermelon 18, strawberry 9, pear 4.5, guava 2.5)
     pointerGrapes     db 'Grapes      $'
@@ -26,6 +26,7 @@
     fruitsName dw offset pointerGrapes, offset pointerApple, offset pointerOrange, offset pointerPapaya, offset pointerWatermelon, offset pointerStrawberry, offset pointerPear, offset pointerGuava
     fruitsIPrice db 7, 2, 1, 5, 18, 9, 4, 2
     fruitsFprice db 0, 20, 80, 0, 0, 0, 50, 50
+    fruitsStock db 50, 50, 50, 50, 50, 50, 50, 50
 
     ; Cart
     cart db 8 dup(0)
@@ -43,10 +44,12 @@
     roundUpI dw 0
     roundUpF dw 0
 
+    ; Print Number Proc (32767 is max value)
+    num dw 0
+
     ; Print Price Proc
-    printPriceI dw 0
-    printPriceF dw 0
-    printPriceTemp db 0
+    printPriceI dw 4
+    printPriceF dw 99
 
 ;==============================================================================
 .code
@@ -56,86 +59,133 @@ main proc
     mov ds, ax
 
     loginLoop:
-        ; Print Fruit Store Title
-        call newline
-        mov dx, offset line
-        mov ah, 09h
-        int 21h
-        call newline
-        mov si, offset appleString
-        mov cx, 5
-        apple:
-            mov dx, [si]
-            mov ah, 09h
-            int 21h
-            call newline
-            add si, 2
-        loop apple
-        mov dx, offset storeName
-        mov ah, 09h
-        int 21h
-        call newline
-        mov dx, offset line
-        mov ah, 09h
-        int 21h
-        call newline
-   
-    ; Print Shutdown Message
+    call fruitStoreTitle
+    
+
+    testing:
+    call newline
+    mov roundUpI, 10
+    mov roundUpF, 9543
+    call roundUp
+    mov ax, roundUpI
+    mov printPriceI, ax
+    mov ax, roundUpF
+    mov printPriceF, ax
+    call printPrice
+    call newline
+
+    exit:
     call newline
     mov dx, offset shutdown
     mov ah, 09h
     int 21h
     call newline
-
-    ; Exit program
     mov ah, 4Ch
     int 21h
 main endp
 
+fruitStoreTitle proc
+call newline
+    mov dx, offset line
+    mov ah, 09h
+    int 21h
+    call newline
+    mov si, offset appleString
+    mov cx, 5
+    apple:
+    mov dx, [si]
+    mov ah, 09h
+    int 21h
+    call newline
+    add si, 2
+    loop apple
+    mov dx, offset storeName
+    mov ah, 09h
+    int 21h
+    call newline
+    mov dx, offset line
+    mov ah, 09h
+    int 21h
+    call newline
+    ret
+fruitStoreTitle endp
+
+printNumber proc
+    mov ax, 1 ; ax is the divisor
+    mov bx, num
+    cmp bx, 10
+    jl singleDigit ; If num < 10, handle as single digit
+
+    testDivisor:
+    mov cx, 10
+    mul cx ; ax *= 10
+    cmp ax, num
+    jg doneDivisor ; Exit loop if divisor exceeds num
+    push ax ; save divisor (10, 100, 1000, 10000)
+    cmp ax, 10000
+    je doneDivisor ; Exit loop if divisor is 10000, since it will overflow
+    jmp testDivisor
+    doneDivisor:
+
+    printLoop:
+    xor dx, dx
+    mov ax, num
+    pop bx ; bx is the divisor
+    div bx ; ax = num / bx, dx = num % bx
+    mov num, dx ; Update num to the remainder
+    mov dx, ax ; print the quotient
+    add dl, 48
+    mov ah, 2
+    int 21h
+    cmp bx, 10 ; If divisor is 10, exit loop
+    jne printLoop
+
+    mov dx, num
+    add dl, 48
+    mov ah, 2
+    int 21h
+    ; Logic behind the loop
+    ; digitOne = num1 / 1000;
+	; num1 = num1 % 1000;
+	; digitTwo = num1 / 100;
+	; num1 = num1 % 100;
+	; digitThree = num1 / 10;
+	; digitFour = num1 % 10;
+    ret
+
+    singleDigit:
+    mov dx, bx
+    add dl, 48
+    mov ah, 2
+    int 21h
+    ret
+printNumber endp
+
 printPrice proc
-    mov printPriceTemp, 0
-    mov cx, printPriceI
-    storeInteger:
-        xor dx, dx
-        mov ax, cx
-        mov bx, 10
-        div bx
-        push dx
-        mov cx, ax
-        inc printPriceTemp
-    loop storeInteger
+    mov ax, printPriceI
+    mov num, ax
+    call printNumber
 
-    mov cx, printPriceTemp
-    printInteger:
-        pop dx
-        add dl, 48
-        mov ah, 02h
-        int 21h
-    loop printInteger
+    mov dl, "."
+    mov ah, 2
+    int 21h
 
-    mov printPriceTemp, 0
-    mov cx, printPriceD
-    storeDecimal:
-        xor dx, dx
-        mov ax, cx
-        mov bx, 10
-        div bx
-        push dx
-        mov cx, ax
-        inc printPriceTemp
-    loop storeDecimal
+    ; Print Floating Part
+    xor dx, dx
+    mov ax, printPriceF
+    mov bx, 10
+    div bx
+    mov printPriceF, dx
+    mov dx, ax
+    add dl, 48
+    mov ah, 2
+    int 21h
+    mov dx, printPriceF
+    add dl, 48
+    mov ah, 2
+    int 21h
 
-    mov ah, 02h
-	mov dl, '.'
-	int 21h
-
-    mov cx, printPriceTemp
-    printDecimal:
-        pop dx
-        add dl, 48
-        mov ah, 02h 
-        int 21h
-    loop printDecimal
+    ret
 printPrice endp
 
 roundUp proc
@@ -145,6 +195,7 @@ roundUp proc
     div bx
     add roundUpI, ax
     mov roundUpF, dx
+    ret
 roundUp endp
 
 newline proc
